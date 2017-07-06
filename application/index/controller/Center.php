4ae26a1071d5b47bcf496718aea8ec6a821ad1e4
SC-201707030920\Administrator <5046312@qq.com>
2017-07-06 16:05:27 +0800

    登陆，退出登陆

---
+++ application/index/controller/Center.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/7/6 0006
+ * Time: 下午 15:48
+ */
+
+namespace app\index\controller;
+
+
+class Center
+{
+
+}
---
+++ application/index/model/User.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/7/6 0006
+ * Time: 下午 15:26
+ */
+class User
+{
+
+}
